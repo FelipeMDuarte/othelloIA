ed86bc55fb606597ad56eb1954d9019c85a2aefa
Fernando Gouvea <fernandosrg1@gmail.com>
2015-06-20 01:51:19 -0300

    Separando o valor dos pesos do mapa de pesos.

--- ZicaMinMax.py
+++ ZicaMinMax.py
@@ -1,21 +1,26 @@
 import minmax
 
+WEIGHT_MAP =   [[6, 7, 8, 9, 9, 8, 7, 6],
+		[7, 3, 4, 5, 5, 4, 3, 7],
+		[8, 4, 1, 2, 2, 1, 4, 8],
+		[9, 5, 2, 0, 0, 2, 5, 9],
+		[9, 5, 2, 0, 0, 2, 5, 9],
+		[8, 4, 1, 2, 2, 1, 4, 8],
+		[7, 3, 4, 5, 5, 4, 3, 7],
+		[6, 7, 8, 9, 9, 8, 7, 6]]
+
+WEIGHTS = [3, 15, 3, -40, -5, -5, 120, -20, 20, 5]
+
+def getWeight(pos):
+	return WEIGHTS[ WEIGHT_MAP[ pos[0] ][ pos[1] ] ]
+
 class ZicaMinMax :
-
-BOARD_WEIGHTS = [[0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
-                 [0, 120, -20,  20,   5,   5,  20, -20, 120,   0],
-                 [0, -20, -40,  -5,  -5,  -5,  -5, -40, -20,   0],
-                 [0,  20,  -5,  15,   3,   3,  15,  -5,  20,   0],
-                 [0,   5,  -5,   3,   3,   3,   3,  -5,   5,   0],
-                 [0,   5,  -5,   3,   3,   3,   3,  -5,   5,   0],
-                 [0,  20,  -5,  15,   3,   3,  15,  -5,  20,   0],
-                 [0, -20, -40,  -5,  -5,  -5,  -5, -40, -20,   0],
-                 [0, 120, -20,  20,   5,   5,  20, -20, 120,   0],
-                 [0,   0,   0,   0,   0,   0,   0,   0,   0,   0]]
-
-	def __init__ ( self , type , key , leaf , depth):
-		minmax.NodeMinMax.__init__( self , type , key , leaf):
-		self.depth = depth
+	def __init__(self, type, key, depth, maxDepth):
+		minmax.NodeMinMax.__init__(self, type, key, depth, maxDepth)
 		self.parent =  None
-		
-		
+	
+	def generateChildren(self):
+		#TODO
+	
+	def calculateValue(self):
+		#TODO
