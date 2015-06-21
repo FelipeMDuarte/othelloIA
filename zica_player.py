ec3c64212bf504263ec0fc67a7fa7c5c00ea9625
Fernando Gouvea <fernandosrg1@gmail.com>
2015-06-21 15:32:10 -0300

    Criando player que usa o ZicaMinMax para decidir suas jogadas.

---
+++ zica_player.py
@@ -0,0 +1,16 @@
+from models.players.ZicaMinMax import ZicaMinMax
+
+class ZicaPlayer:
+	def __init__(self, color):
+		self.color = color
+		self.minMaxTreeDepth = 3
+
+	def play(self, board):
+		minMax = ZicaMinMax(1, None, 1, self.minMaxTreeDepth, board, self.color)
+		minMax.evaluate()
+		
+		for child in minMax.children:
+			if child.value == minMax.value:
+				return child.key
+		
+		return None
