d7a3c8eb7328e78888e4f3873888e7eb924eb7ef
Fernando Gouvea <fernandosrg1@gmail.com>
2015-06-21 00:16:14 -0300

    Implementando calculateValue() do ZicaMinMax para calcular o valor do tabuleiro de um nó.

--- ZicaMinMax.py
+++ ZicaMinMax.py
@@ -1,4 +1,6 @@
-import minmax
+import models.players.minmax as minmax
+from models.players.minmax import NodeMinMax
+from models.board import Board
 
 WEIGHT_MAP =   [[6, 7, 8, 9, 9, 8, 7, 6],
 		[7, 3, 4, 5, 5, 4, 3, 7],
@@ -14,13 +16,26 @@ WEIGHTS = [3, 15, 3, -40, -5, -5, 120, -20, 20, 5]
 def getWeight(pos):
 	return WEIGHTS[ WEIGHT_MAP[ pos[0] ][ pos[1] ] ]
 
-class ZicaMinMax :
-	def __init__(self, type, key, depth, maxDepth):
+class ZicaMinMax (NodeMinMax):
+	def __init__(self, type, key, depth, maxDepth, board, color):
 		minmax.NodeMinMax.__init__(self, type, key, depth, maxDepth)
 		self.parent =  None
+		self.board = board
+		self.color = color
+		self.enemyColor = 'o' if color == '@' else '@'
 	
 	def generateChildren(self):
 		#TODO
+		return 0
 	
 	def calculateValue(self):
-		#TODO
+		self.value = 0
+
+		for i in range(1,9):
+			for j in range(1,9):
+				if self.board.board[i][j] == self.color:
+					weight = WEIGHT_MAP[i-1][j-1]
+					self.value += WEIGHTS[weight]
+				elif self.board.board[i][j] == self.enemyColor:
+					weight = WEIGHT_MAP[i-1][j-1]
+					self.value -= WEIGHTS[weight]
